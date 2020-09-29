21dfab5abc21711adbc78e32c0c2cba1fc1a913d
Michael <micjanic20@gmail.com>
2020-09-28 20:49:20 -0500

    schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
-const express = require("express");
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
 
-const app = express();
+const app = express()
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+  })
+)
 
 app.listen(4000, () => {
-  console.log("now listening on port 4000");
-});
+  console.log('now listening on port 4000')
+})
